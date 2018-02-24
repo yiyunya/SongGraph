086c33938738ab20836c1692292cf75c68f87b21
yiyunya <yiyunya@users.noreply.github.com>
2018-02-24 18:40:46 +0800

    data preprocessing

--- test.py
+++ test.py
@@ -6,3 +6,14 @@
 #     print(a[item])
 #
 #
+
+# a = {'f':{'b':1}}
+# b = {'f':[{'b':1},{'c':2}]}
+#
+#
+# print(type(a['f']))
+# print(type(b['f']))
+
+
+a={'b':""}
+print(a['b'] is not 0 or None)
--- traversal/utils.py
+++ traversal/utils.py
@@ -26,16 +26,110 @@ def dump_dict(data):
     basic_info_list = ["PersonId", "EngName", "ChName", "IndexYear", "Gender", "YearBirth", "YearDeath", "Dynasty"]
     d['BasicInfo']=simplify(basic_info,basic_info_list)
 
+    address_info_list = ["AddrType", "AddrName", "belongs1_name", "belongs2_name", "belongs3_name"]
+    if data['Package']['PersonAuthority']['PersonInfo']['Person']["PersonAddresses"] is not "":
+        address_info = data['Package']['PersonAuthority']['PersonInfo']['Person']["PersonAddresses"]["Address"]
+
+        flag = 0
+        if isinstance(address_info,dict):
+            d['Address']=simplify(address_info,address_info_list)
+            flag = 1
+        elif isinstance(address_info,list):
+            for i in address_info:
+                if i["AddrType"]=="籍貫(基本地址)":
+                    d['Address'] = simplify(i, address_info_list)
+                    flag = 1
+                    break
+        else:
+            for item in address_info_list:
+                d['Address'][item] = 'Nan'
+            flag = 1
+        if flag == 0:
+            for item in address_info_list:
+                d['Address'][item] = 'Nan'
+            flag = 1
+    else:
+        for item in address_info_list:
+            d['Address'][item] = 'Nan'
+
+    entry_info_list = ["RuShiDoor", "RuShiType", "RuShiYear"]
+    if data['Package']['PersonAuthority']['PersonInfo']['Person']["PersonEntryInfo"] is not "":
+        entry_info = data['Package']['PersonAuthority']['PersonInfo']['Person']["PersonEntryInfo"]["Entry"]
+
+        if isinstance(address_info,dict):
+            d['Entry']=simplify(entry_info,entry_info_list)
+        else:
+            for item in entry_info_list:
+                d['Entry'][item]='Nan'
+    else:
+        for item in entry_info_list:
+            d['Entry'][item] = 'Nan'
+
+
+    status_info_list = ["StatusName"]
+    if data['Package']['PersonAuthority']['PersonInfo']['Person']["PersonSocialStatus"] is not "":
+        status_info = data['Package']['PersonAuthority']['PersonInfo']['Person']["PersonSocialStatus"]["SocialStatus"]
+        if isinstance(status_info,dict):
+            d['Status']=[simplify(status_info,status_info_list)]
+        elif isinstance(status_info,list):
+            l = []
+            for i in status_info:
+                l.append(simplify(i,status_info_list))
+            d['Status']=l
+    else:
+        tmp = {}
+        for item in status_info_list:
+            tmp[item]='Nan'
+        d['Status']=[tmp]
+
+    kin_info_list = ["KinPersonId", "KinPersonName", "KinCode", "KinRel", "KinRelName"]
+    if data['Package']['PersonAuthority']['PersonInfo']['Person']["PersonKinshipInfo"] is not "":
+        kin_info = data['Package']['PersonAuthority']['PersonInfo']['Person']["PersonKinshipInfo"]["Kinship"]
+        if isinstance(kin_info, dict):
+            d['Kinship'] = [simplify(kin_info,kin_info_list)]
+        elif isinstance(kin_info,list):
+            l = []
+            for i in kin_info:
+                l.append(i,kin_info_list)
+            d['Kinship']=l
+    else:
+        tmp = {}
+        for item in kin_info_list:
+            tmp[item]='Nan'
+        d['Kinship']=[tmp]
+
+    social_info_list = ["AssocPersonId", "AssocPersonName", "AssocCode", "AssocName"]
+    if data['Package']['PersonAuthority']['PersonInfo']['Person']["PersonSocialAssociation"] is not "":
+        social_info = data['Package']['PersonAuthority']['PersonInfo']['Person']["PersonSocialAssociation"]["Association"]
+
+
+
+
+
+
+
+
+
+
+
+
+
     # d['BasicInfo']={"PersonId":bi["PersonId"], "EngName":bi["EngName"], "ChName":bi["ChName"],
     #                 "IndexYear":bi["IndexYear"], "Gender":bi["Gender"], "YearBirth":bi["YearBirth"],
     #                 "YearDeath":bi["YearDeath"], "Dynasty":bi["Dynasty"]}
     #
     return d
 
+
+
+
 def simplify(data,list):
     d = {}
     for item in list:
-        d[item]=data[item]
+        if (item in d.keys()) and (data[item] is not 0 or "未知" or "未詳" or None or ""):
+            d[item]=data[item]
+        else:
+            d[item]='Nan'
     return d
 
 
